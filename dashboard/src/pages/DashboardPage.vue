907b5aaeab05e20f24e4b6918b224ba1c14f67f1
Devin Holloway <devin@shinynet.com>
2023-11-16 02:19:59 -0700

    HL-19: enabled binary features of devices

--- dashboard/src/pages/DashboardPage.vue
+++ dashboard/src/pages/DashboardPage.vue
@@ -1,11 +1,31 @@
 <template>
-  <q-page padding>
-    <!-- content -->
+  <q-page padding class="grid">
+    <device-card v-for="device in devices" :key="device.ref" v-bind="device"/>
   </q-page>
 </template>
 
-<script>
-export default {
-  // name: 'PageName',
-}
+<script setup>
+import { api } from 'boot/axios'
+import { useQuery } from '@tanstack/vue-query'
+import DeviceCard from 'components/DeviceCard.vue'
+
+const { data: devices } = useQuery({
+  queryFn: () => api.get('JSON', {
+    params: {
+      request: 'getstatus',
+      everything: true,
+      ref: '74,75,6,37'
+    }
+  })
+    .then(({ data }) => data.Devices)
+    .then(devices => devices.map(device => ({ ...device, id: device.ref }))),
+  queryKey: ['devices']
+})
 </script>
+
+<style scoped>
+.grid {
+  display: grid;
+  gap: 16px;
+}
+</style>
