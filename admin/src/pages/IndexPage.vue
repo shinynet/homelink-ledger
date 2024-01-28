c4101aed9831a2e4e637d668ec8685119d73e6d1
Devin <devin@shinynet.com>
2024-01-27 17:12:39 -0700

    HL-52 Create UI for pre-minting admin tokens

--- admin/src/pages/IndexPage.vue
+++ admin/src/pages/IndexPage.vue
@@ -1,17 +1,93 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page
+    class="column justify-center"
+    padding>
+    <q-form
+      class="content-around column"
+      @submit="handleSubmit">
+      <q-input
+        ref="licenseKeyRef"
+        v-model="licenseKey"
+        :rules="validation"
+        dense
+        filled
+        mask="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+        maxlength="36"
+        style="min-width: 330px">
+        <template v-slot:after>
+          <q-btn
+            color="accent"
+            dense
+            icon="key"
+            size="md"
+            @click="handleGenKey"/>
+        </template>
+      </q-input>
+
+      <q-btn
+        color="primary"
+        label="Mint Admin Token"
+        type="submit"/>
+    </q-form>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { defineComponent, nextTick, onBeforeUnmount, ref } from 'vue'
+import { uid, useQuasar } from 'quasar'
+import { useMintToken } from 'src/composables/mintToken'
 
-export default defineComponent({
-  name: 'IndexPage'
+defineComponent({ name: 'index-page' })
+
+const { loading, notify } = useQuasar()
+const mint = useMintToken()
+
+const licenseKey = ref('')
+const licenseKeyRef = ref(null)
+
+const validation = [
+  value => !!value || 'License Key is required',
+  value => value.length === 36 || 'License Key must be 36 characters'
+]
+
+const handleGenKey = () => {
+  licenseKey.value = uid()
+}
+
+const handleSubmit = () => {
+  loading.show({
+    message: 'Minting Admin Token. Please wait...',
+    boxClass: 'bg-primary'
+  })
+
+  mint(licenseKey)
+    .then(() => {
+      notify({
+        progress: true,
+        message: 'Admin token minted successfully',
+        type: 'positive',
+        multiLine: true
+      })
+    })
+    .catch(error => {
+      notify({
+        progress: true,
+        message: error.info ?? error,
+        type: 'negative',
+        multiLine: true
+      })
+    })
+    .finally(() => {
+      licenseKey.value = ''
+      nextTick()
+        .then(() => {
+          licenseKeyRef.value.resetValidation()
+          loading.hide()
+        })
+    })
+}
+
+onBeforeUnmount(() => {
+  loading.hide()
 })
 </script>
