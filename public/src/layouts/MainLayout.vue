35d716a0fbac7fbbb34ba0c01ec3ff3fc8aefc87
Devin <devin@shinynet.com>
2024-03-01 01:28:06 -0700

    Home page

--- public/src/layouts/MainLayout.vue
+++ public/src/layouts/MainLayout.vue
@@ -1,17 +1,27 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh Lpr lff">
 
     <q-header>
       <q-toolbar>
         <q-toolbar-title>
           <q-img src="logo.png" style="width: 180px; height: 75px"/>
         </q-toolbar-title>
+
+        <q-btn dense flat icon="shopping_cart" round/>
       </q-toolbar>
     </q-header>
 
     <q-page-container>
       <router-view/>
     </q-page-container>
+
+    <q-footer class="bg-grey-3 text-dark">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-btn flat>Terms & Service</q-btn>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
--- public/src/pages/IndexPage.vue
+++ public/src/pages/IndexPage.vue
@@ -1,110 +1,73 @@
 <template>
-  <q-page class="flex flex-center column">
-    <q-parallax src="hero.png">
+  <q-page class="flex-center column q-gutter-lg">
+    <q-parallax :height="740" src="hero.png">
       <template v-slot:content>
-        <div class="flex flex-center q-px-xl hero-content-bg">
-          <h1 class="text-accent text-weight-regular text-center text-italic hero-text">
+        <div class="q-px-xl hero-content-bg">
+          <h1 class="text-accent text-weight-regular text-center text-italic hero-text text-h2">
             Protect your home with the power of blockchain
           </h1>
         </div>
       </template>
     </q-parallax>
 
-    <q-list bordered separator style="width: 500px">
-      <q-item>
-        <q-item-section>
-          <q-item-label class="text-italic">“Revolutionary Convenience!”</q-item-label>
-          <q-item-label caption>The Homelink Ledger has transformed my daily routine. With its intuitive interface and
-            robust compatibility, I can control everything from my lights to my thermostat with a simple voice
-            command.
-            Plus, the blockchain technology ensures security and transparency. Highly recommended! 🏡🔒
-          </q-item-label>
-        </q-item-section>
-        <q-item-section side top>
-          <div class="text-orange">
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-          </div>
-        </q-item-section>
-      </q-item>
-
-      <q-item>
-        <q-item-section>
-          <q-item-label>“Effortless Integration”</q-item-label>
-          <q-item-label caption>As a tech enthusiast, I’ve tried various home automation systems, but the Homelink
-            Ledger stands out. The seamless integration of devices across different brands is impressive. Whether it’s
-            adjusting the blinds or setting up a morning routine, this system makes life easier. 🤖🏠
-          </q-item-label>
-        </q-item-section>
-        <q-item-section side top>
-          <div class="text-orange">
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-          </div>
-        </q-item-section>
-      </q-item>
-
-      <q-item>
-        <q-item-section>
-          <q-item-label>“Smart Home Bliss”</q-item-label>
-          <q-item-label caption>Homelink Ledger has turned my house into a smart home paradise. The blockchain
-            backbone
-            ensures data security, and the user-friendly app simplifies control. I love how it adapts to my
-            preferences
-            and even anticipates my needs. It’s like having a personal assistant for my home! 🌟🏡
-          </q-item-label>
-        </q-item-section>
-        <q-item-section side top>
-          <div class="text-orange">
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-          </div>
-        </q-item-section>
-      </q-item>
-
-      <q-item>
-        <q-item-section>
-          <q-item-label>“Worth Every Penny”</q-item-label>
-          <q-item-label caption>Initially, I was hesitant about the price, but the Homelink Ledger has exceeded my
-            expectations. The investment is well worth it. From energy efficiency to enhanced security, it’s a
-            game-changer. Plus, the customer support team is responsive and helpful. Thumbs up! 💰👍
-          </q-item-label>
-        </q-item-section>
-        <q-item-section side top>
-          <div class="text-orange">
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-            <q-icon name="star"/>
-          </div>
-        </q-item-section>
-      </q-item>
-    </q-list>
-
-    <div>
-      Remember, the Homelink Ledger isn’t just a home automation system; it’s a lifestyle upgrade. Get ready to
-      experience the future of smart living!
+    <div class="row q-pa-md-xl">
+      <div class="col-12 col-md">
+        <q-list separator>
+          <q-item v-for="{id, title, rating, content} in reviews" :key="id">
+            <q-item-section>
+              <q-item-label class="text-italic text-subtitle1">{{ title }}</q-item-label>
+              <q-item-label class="text-body2 q-pa-md">{{ content }}</q-item-label>
+            </q-item-section>
+            <q-item-section class="gt-xs" side top>
+              <div class="text-orange">
+                <q-icon v-for="(_, i) in rating" :key="i" name="star"/>
+              </div>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+      <div class="col-12 col-md q-pa-md-xl text-h6 text-center text-italic">
+        Homelink Ledger isn’t just a home automation system, it’s a lifestyle upgrade. Get ready to experience the
+        future of smart living!
+        <div class="q-pa-lg">
+          <q-btn color="accent" size="lg">Order Now</q-btn>
+        </div>
+      </div>
     </div>
 
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+defineOptions({ name: 'index-page' })
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+const reviews = [
+  {
+    id: 1,
+    title: 'Revolutionary Convenience!',
+    rating: 5,
+    content: 'Homelink Ledger has transformed my daily routine. With its intuitive interface and robust compatibility, I can control everything from my lights to my thermostat with a simple voice command. Plus, the blockchain technology ensures security and transparency. Highly recommended! 🏡🔒'
+  },
+  {
+    id: 2,
+    title: 'Effortless Integration',
+    rating: 5,
+    content: 'As a tech enthusiast, I\'ve tried various home automation systems, but Homelink Ledger stands out. The seamless integration of devices across different brands is impressive. Whether it\'s adjusting the blinds or setting up a morning routine, this system makes life easier. 🤖🏠'
+  },
+  {
+    id: 3,
+    title: 'Smart Home Bliss',
+    rating: 5,
+    content: 'Homelink Ledger has turned my house into a smart home paradise. The blockchain backbone ensures data security, and the user-friendly app simplifies control. I love how it adapts to my preferences and even anticipates my needs. It\'s like having a personal assistant for my home! 🌟🏡'
+  },
+  {
+    id: 4,
+    title: 'Worth Every Penny',
+    rating: 5,
+    content: 'Initially, I was hesitant about the price, but Homelink Ledger has exceeded my expectations. The investment is well worth it. From energy efficiency to enhanced security, it\'s a game-changer. Plus, the customer support team is responsive and helpful. Thumbs up! 💰👍'
+  }
+]
+
 </script>
 
 <style lang="scss" scoped>
